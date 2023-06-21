ed81d7e3702e9128801b0dc8352a902a10d9339d
dsllt <dayanesallet@hotmail.com>
2023-06-21 19:54:11 -0300

    Passa informações do usuário na session

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -40,6 +40,13 @@ export function buildNextAuthOptions(
         }
         return true
       },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
   }
 }
