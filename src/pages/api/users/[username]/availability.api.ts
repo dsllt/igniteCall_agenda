e11f0242c235d4bc23a963f2f458616bb7ae9acd
dsllt <dayanesallet@hotmail.com>
2023-06-29 05:34:06 -0300

    Exibindo os horários disponíveis no formulário

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -32,7 +32,7 @@ export default async function handler(
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
 
   if (isPastDate) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const userAvailability = await prisma.userTimeInterval.findFirst({
@@ -43,7 +43,7 @@ export default async function handler(
   })
 
   if (!userAvailability) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -11,9 +11,14 @@ import dayjs from 'dayjs'
 import { api } from '@/lib/axios'
 import { useRouter } from 'next/router'
 
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
+
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState(null)
+  const [availability, setAvailability] = useState<Availability | null>(null)
 
   const router = useRouter()
   const username = String(router.query.username)
@@ -37,7 +42,7 @@ export function CalendarStep() {
         },
       })
       .then((response) => {
-        console.log(response.data)
+        setAvailability(response.data)
       })
   }, [selectedDate, username])
 
@@ -50,22 +55,16 @@ export function CalendarStep() {
             {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
           <TimePickerList>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
-            <TimePickerButton>12:00h</TimePickerButton>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <TimePickerButton
+                  key={hour}
+                  disabled={!availability.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, '0')}:00h
+                </TimePickerButton>
+              )
+            })}
           </TimePickerList>
         </TimePicker>
       )}
